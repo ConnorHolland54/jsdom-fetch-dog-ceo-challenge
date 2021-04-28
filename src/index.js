2f6607c1b38fc9177344b4226b4099b75a7f32bf
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-28 14:14:15 -0600

    commit tree c0b788bf0e563d597a4f30e2efd2c2a41867cfec

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchImages(imgUrl)
+
+})
+
+function fetchImages(url) {
+  fetch(url) .then(function(response) {
+  return response.json();
+}) .then(function(json){
+  addImageToPage(json)
+})
+}
+
+function addImageToPage(images) {
+  let imageContainer = document.getElementById('dog-image-container')
+  for (const url of images['message']) {
+    let newImage = document.createElement('img')
+    newImage.src = url
+    newImage.width = '100'
+    newImage.heigth = '100'
+    imageContainer.appendChild(newImage)
+  }
+}
